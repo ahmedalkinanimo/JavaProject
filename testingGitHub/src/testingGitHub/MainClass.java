18d58b10162b2131e6bb9d17ab76ce2290d82fa3
ahmedalkinanimo <aaljanabi@kckcc.edu>
2023-03-07 12:59:19 -0600

    First Commit (3/7/2023)1:00pm

---
+++ testingGitHub/src/testingGitHub/MainClass.java
@@ -0,0 +1,11 @@
+package testingGitHub;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Testing for the 10th times");
+	}
+
+}
